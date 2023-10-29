a8fc0cf7da678aa0198f49c3eaa7737508e7a591
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-10-29 15:27:42 -0700

    Add desync module with Golang dependency

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+def can_build(env, platform):
+    return True
+
+def configure(env):
+    try:
+        go_version = subprocess.check_output(["go", "version"])
+        print("Golang is installed: ", go_version)
+    except Exception as e:
+        print("Golang is not installed or not found in PATH")
+        return False
+    return True
